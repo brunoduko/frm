7a80d633886f76276c1bf4afb10893fe8a7f6c1b
brunoduko <55922129+brunoduko@users.noreply.github.com>
2020-02-16 22:18:52 -0600

    Add files via upload

---
+++ PA1_T1_Bruno_PalaciosTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PA1_T1_Bruno_Palacios;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author usuario
+ */
+public class PA1_T1_Bruno_PalaciosTest {
+    
+    public PA1_T1_Bruno_PalaciosTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+}
